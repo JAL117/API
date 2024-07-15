f5668cff7e8b8d724c65702ddd8a7837aef1c881
Jal <106935663+JAL117@users.noreply.github.com>
2024-07-15 09:12:56 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("pruebaapi", "admin", "michi123123", {
-  host: "database-1.cf8uui4k0410.us-east-1.rds.amazonaws.com",
+const sequelize = new Sequelize("pruebaapi", "admin", "lolitaayala", {
+  host: "pruebaapi.ccevjhrbihxq.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 
